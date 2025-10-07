d8a55338f31c77fb303ff39c1e023569a7148000
SorenKyhl <soren@kyhl.com>
2025-10-07 02:17:05 -0500

    Update __init__.py
    load gui functions

--- labs/L3/menglab3/menglab3/__init__.py
+++ labs/L3/menglab3/menglab3/__init__.py
@@ -6,11 +6,36 @@ from .load import (
     generate_kmeans_data
 )
 
+from .gui import (
+    interactive_projection,
+    interactive_PCA_rotation,
+    interactive_PCA_projection,
+    scree_plot,
+    pca_scatter,
+    elbow_plot,
+    silhouette_scan,
+    interactive_2d_projection,
+    PCA_projection_3D,
+    interactive_k_means
+)
+
+
 __all__ = [
     # load
     "generate_projection_data", 
     "generate_PCA_data",
-    "generate_kmeans_data"
+    "generate_kmeans_data",
+    # gui
+    "interactive_projection", 
+    "interactive_PCA_rotation",
+    "interactive_PCA_projection",
+    "scree_plot",
+    "pca_scatter", 
+    "elbow_plot",
+    "silhouette_scan",
+    "interactive_2d_projection",
+    "PCA_projection_3D",
+    "interactive_k_means"
 ]
 
 # Optional: add version metadata
